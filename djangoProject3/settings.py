f8b0d5a5692bcfb4161afb3eb4fd8bd0f2834d35
Дмитрий <vasja8102@mail.ru>
2024-01-25 19:10:03 +0300

    Prodoljenie

--- djangoProject3/settings.py
+++ djangoProject3/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'djangoProject3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.psycopg2',
+        'NAME': 'parcing_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+
     }
 }
 
