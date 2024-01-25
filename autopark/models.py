916d03d301f79826afa88f7421a86f8dfa54c6b7
Дмитрий <vasja8102@mail.ru>
2024-01-25 19:56:21 +0300

    Prodoljenie-3

--- autopark/models.py
+++ autopark/models.py
@@ -5,9 +5,14 @@ from django.db import models
 class CarType(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ['name']
+
 class CarBrand(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        ordering = ['name']
 
 class Car(models.Model):
     car_number = models.CharField(max_length=10)
@@ -21,6 +26,10 @@ class ParkingSlot(models.Model):
    is_free = models.BooleanField(default=True)
    number = models.IntegerField()
    car = models.OneToOneField(Car, on_delete=models.SET_NULL, null=True)
+
+class Meta:
+    ordering = ['number']
+
 class Parking(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
